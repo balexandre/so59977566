67a464590556207ce239379175dfb8fdd2afcba1
Bruno Alexandre <bruno.in.dk@gmail.com>
2020-02-01 16:36:45 +0100

    send json

--- server/routes/advanced.js
+++ server/routes/advanced.js
@@ -3,7 +3,7 @@ const calculator = require('./../utilities/calculator');
 
 const circumference = (req, res) => {
     const result = calculator.multiply(req.params.a, 2);
-    res.send(calculator.multiply(result, Math.PI));
+    res.send({ result: calculator.multiply(result, Math.PI) });
 };
 
 router.get('/circumference/:a', circumference);
