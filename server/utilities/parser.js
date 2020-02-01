02feeb748adf12ae61b0a6aad8dc5ee354398878
Bruno Alexandre <bruno.in.dk@gmail.com>
2020-02-01 16:36:40 +0100

    no need try/catch

--- server/utilities/parser.js
+++ server/utilities/parser.js
@@ -1,12 +1,8 @@
 // simple parser as all inputs are received as string
 
 const parseInput = input => {
-    try {
-        const result = parseFloat(input);
-        return isNaN(result) || !isFinite(result) ? 0 : result;
-    } catch (err) {
-        return 0;
-    }
+    const result = parseFloat(input);
+    return isNaN(result) || !isFinite(result) ? 0 : result;
 }
 
 
